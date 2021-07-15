5fd856abe75dac0993abdf7b3c7b9a2d8f20129c
yaya <yaya@xyyaya.com>
2021-07-16 00:48:10 +0800

    fix: 修复 buildSQL 参数 limit 和 offset 造成的错误返回

--- src/lib/DatabaseComponent.ts
+++ src/lib/DatabaseComponent.ts
@@ -1,5 +1,5 @@
 import {Component, IComponentOptions, Runtime, Utility} from '@sora-soft/framework';
-import {createConnection, ConnectionOptions, Connection, EntityTarget, OrderByCondition} from 'typeorm';
+import {createConnection, ConnectionOptions, Connection, EntityTarget, OrderByCondition, QueryBuilder} from 'typeorm';
 import {MysqlConnectionOptions} from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import {DatabaseError} from './DatabaseError';
 import {DatabaseErrorCode} from './DatabaseErrorCode';
@@ -12,14 +12,25 @@ export interface IDatabaseComponentOptions extends IComponentOptions {
   database: ConnectionOptions;
 }
 
-export interface ISqlOptions<Entity = any> {
+export interface IRelationsSqlOptions<Entity = any> {
   select?: string[];
-  relations?: string[];
+  relations: string[];
+  innerJoinKey: string;
   order?: OrderByCondition,
   offset?: number;
   limit?: number;
   where?: WhereCondition<Entity>;
 }
+
+export interface INoRelationsSqlOptions<Entity = any> {
+  select?: string[];
+  order?: OrderByCondition,
+  offset?: number;
+  limit?: number;
+  where?: WhereCondition<Entity>;
+}
+
+export type ISqlOptions<Entity = any> = INoRelationsSqlOptions<Entity> & IRelationsSqlOptions<Entity>;
 
 class DatabaseComponent extends Component {
 
@@ -47,12 +58,77 @@ class DatabaseComponent extends Component {
     this.connection_ = null;
   }
 
+  prepareQuery(query: QueryBuilder<any>) {
+    const [sql, params] = query.getQueryAndParameters();
+    let finalSQL = sql;
+    const parameters: any = {};
+
+    const m = sql.match(/\?/g);
+    if (m === null) {
+      return {sql, parameters};
+    }
+    let index = 0;
+    let value = null;
+    for (const m0 of m) {
+      switch (typeof params[index]) {
+        case 'string':
+          value = `'${params[index]}'`;
+          break;
+
+        default:
+          value = params[index];
+          break;
+      }
+
+      finalSQL = finalSQL.replace(m0, `:value${index}`);
+      parameters[`value${index}`] = params[index];
+      index++;
+    }
+
+    return {
+      sql: finalSQL,
+      parameters,
+    };
+  };
+
   buildSQL<T = any>(entity: EntityTarget<T>, options: ISqlOptions<T>) {
     let sqlBuilder = this.manager.getRepository(entity).createQueryBuilder('self');
-    if (options.relations) {
+    if (options.relations && options.relations.length) {
+      const innerJoinBuilder = this.manager.getRepository(entity).createQueryBuilder();
+      innerJoinBuilder.select(options.innerJoinKey);
+      if (options.limit) {
+        innerJoinBuilder.limit(options.limit);
+      }
+      if (options.offset) {
+        innerJoinBuilder.offset(options.offset);
+      }
+      if (options.where) {
+        innerJoinBuilder.where(WhereBuilder.build(options.where));
+      }
+      if (options.order) {
+        innerJoinBuilder.orderBy(options.order);
+      }
+
+      const {sql, parameters} = this.prepareQuery(innerJoinBuilder);
+      sqlBuilder.innerJoin(`(${sql})`, 'inner', `inner.${options.innerJoinKey} = self.${options.innerJoinKey}`);
+      sqlBuilder.setParameters(parameters);
+
       options.relations.forEach((relation) => {
         sqlBuilder = sqlBuilder.leftJoinAndSelect(`self.${relation}`, relation);
       });
+    } else {
+      if (options.where) {
+        sqlBuilder = sqlBuilder.where(WhereBuilder.build(options.where));
+      }
+      if (options.order) {
+        sqlBuilder = sqlBuilder.orderBy(options.order);
+      }
+      if (options.limit) {
+        sqlBuilder = sqlBuilder.limit(options.limit);
+      }
+      if (options.offset) {
+        sqlBuilder = sqlBuilder.offset(options.offset);
+      }
     }
     if (options.select) {
       const select = options.select.map((s) => {
@@ -64,18 +140,7 @@ class DatabaseComponent extends Component {
       });
       sqlBuilder = sqlBuilder.select(select);
     }
-    if (options.where) {
-      sqlBuilder = sqlBuilder.where(WhereBuilder.build(options.where));
-    }
-    if (options.order) {
-      sqlBuilder = sqlBuilder.orderBy(options.order);
-    }
-    if (options.limit) {
-      sqlBuilder = sqlBuilder.limit(options.limit);
-    }
-    if (options.offset) {
-      sqlBuilder = sqlBuilder.offset(options.offset);
-    }
+
     return sqlBuilder;
   }
 
